ca85423c73cfd03d26bf24006257e597f0008e53
RVCDv <101155145+weeeeee8@users.noreply.github.com>
2023-08-26 18:18:08 +0800

    Update builtIn.lua

--- cmd/builtIn.lua
+++ cmd/builtIn.lua
@@ -1,4 +1,5 @@
 local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
 local TeleportService = game:GetService("TeleportService")
 local UserInputService = game:GetService("UserInputService")
 
@@ -9,20 +10,43 @@ local IMPORTED_PACKAGES = {}
 local IGNORE_PACKAGES = {'builtIn'}
 GLOBAL.GenericKeybinds = {
     Teleport = Enum.KeyCode.T,
+    Fly = Enum.KeyCode.F,
 }
 
-if GLOBAL.GenericConnections and #GLOBAL.GenericConnections then
+if GLOBAL.GenericCleanup and #GLOBAL.GenericCleanup then
     for i = 1, #GLOBAL.GenericConnections do
-        GLOBAL.GenericConnections[i]:Disconnect()
+        local cleanup = GLOBAL.GenericCleanup[i]
+        if type(cleanup) == "function" then
+            cleanup()
+        elseif typeof(cleanup) == "Instance" or (type(cleanup) == "table" and type(cleanup.Destroy) == "function") then
+            cleanup:Destroy()
+        elseif typeof(cleanup) == "RBXScriptConnection" or (type(cleanup) == "table" and type(cleanup.Disconnect) == "function") then
+            cleanup:Disconnect()
+        end
     end
 end
-GLOBAL.GenericConnections = {}
+GLOBAL.GenericCleanup = {}
 
 local require = GLOBAL.require
 local CommandsAPIService = GLOBAL.CommandsAPIService
 local MakeChatSystemMessage = GLOBAL.MakeChatSystemMessage
 
+local flightStateEnabled = false
+local flyInputs = {
+    [Enum.KeyCode.A] = false,
+    [Enum.KeyCode.S] = false,
+    [Enum.KeyCode.W] = false,
+    [Enum.KeyCode.D] = false,
+}
+local flyMovers = {
+    position = nil :: AlignPosition,
+    orientation = nil :: AlignOrientation,
+}
+local flightSpeed = 16
+
 local canPlayerTeleportToMouse = true
+local canPlayerFly = true
+
 local humanoidRootPart = nil
 local humanoid = nil
 
@@ -34,9 +58,43 @@ local function RaycastToMouse(distance: number?): {Position: Vector3, Normal: Ve
     return if result then result else {Instance = nil, Position = ray.Origin + dir, Normal = Vector3.yAxis, Material = Enum.Material.Air, Distance = 0}
 end
 
+local function toggleFlyState(toggled: boolean)
+    if canPlayerFly then
+        if toggled then
+            flyMovers.position.Position = humanoidRootPart.Position
+            flyMovers.position.Enabled = true
+            flyMovers.orientation.CFrame = CFrame.lookAt(Vector3.zero, workspace.CurrentCamera.CFrame.LookVector)
+            flyMovers.orientation.Enabled = true
+        else
+            flyMovers.position.Enabled = false
+            flyMovers.orientation.Enabled = false
+        end
+        flightStateEnabled = toggled
+    end
+end
+
 local function OnCharacterAdded(character: Model)
     humanoid = character:WaitForChild("Humanoid") :: Humanoid
     humanoidRootPart = character:WaitForChild("HumanoidRootPart") :: Part
+
+    local attachment = Instance.new("Attachment")
+    attachment.Parent = humanoidRootPart
+    local alignPosition = Instance.new("AlignPosition")
+    alignPosition.Attachment0 = attachment
+    alignPosition.MaxVelocity = 10e5
+    alignPosition.MaxForce = math.huge
+    alignPosition.Mode = Enum.PositionAlignmentMode.OneAttachment
+    alignPosition.Enabled = false
+    alignPosition.Parent = humanoidRootPart
+    flyMovers.position = alignPosition
+    local alignOrientation = Instance.new("AlignOrientation")
+    alignOrientation.Attachment0 = attachment
+    alignOrientation.MaxAngularVelocity = 10e5
+    alignOrientation.MaxTorque = math.huge
+    alignOrientation.Mode = Enum.OrientationAlignmentMode.OneAttachment
+    alignOrientation.Enabled = false
+    alignOrientation.Parent = humanoidRootPart
+    flyMovers.orientation = alignOrientation
 end
 
 GLOBAL.RaycastToMouse = RaycastToMouse
@@ -99,12 +157,61 @@ CommandsAPIService.PostCommand {
 }
 
 CommandsAPIService.PostCommand {
+    Name = "flyenabled",
+    Description = "A toggable where you can enable your character to enter flight mode whenever you pressed the fly keybind",
+    Callback = function(out: boolean)
+        canPlayerFly = out
+    end,
+    Arguments = {out = "boolean"}
+}
+
+CommandsAPIService.PostCommand {
     Name = "tpenabled",
-    Description = "A toggable where you can teleport to your mouse position whenever you pressed the teleport keybind (Default: T)",
+    Description = "A toggable where you can enable your character teleport to your mouse position whenever you pressed the teleport keybind",
     Callback = function(out: boolean)
         canPlayerTeleportToMouse = out
     end,
     Arguments = {out = "boolean"}
+}
+
+CommandsAPIService.PostCommand {
+    Name = "settflightspeed",
+    Description = "Adjust the flight speed to your liking (min: 16)",
+    Callback = function(speed: boolean)
+        if speed < 16 then
+            MakeChatSystemMessage.Out("Cannot have flight speed below 16", MakeChatSystemMessage.Colors[2])
+        end
+        flightSpeed = math.max(speed, 16)
+    end,
+    Arguments = {speed = "number"}
+}
+
+CommandsAPIService.PostCommand {
+    Name = "showboundedkeybinds",
+    Description = "Display all bounded keybinds",
+    Callback = function()
+        local genericKeybindList = {}
+        for key, code in pairs(GLOBAL.GenericKeybinds) do
+            table.insert(genericKeybindList, ("%s - %q"):format(key, tostring(code)))
+        end
+        return ("Current bounded keybinds: \n%q"):format(table.concat(genericKeybindList, '\n'))
+    end,
+}
+
+CommandsAPIService.PostCommand {
+    Name = "fly",
+    Description = "Set flight mode enabled",
+    Callback = function()
+        toggleFlyState(true)
+    end
+}
+
+CommandsAPIService.PostCommand {
+    Name = "unfly",
+    Description = "Set flight mode disabled",
+    Callback = function()
+        toggleFlyState(false)
+    end
 }
 
 CommandsAPIService.PostCommand {
@@ -114,7 +221,7 @@ CommandsAPIService.PostCommand {
         if GLOBAL.GenericKeybinds[of] == nil then
             local genericKeybindList = {}
             for key in pairs(GLOBAL.GenericKeybinds) do
-                table.insert(genericKeybindList, tostring(key))
+                table.insert(genericKeybindList, key)
             end
             error(("%s is not a bound keybind, bounded keys are:\n%q"):format(of, table.concat(genericKeybindList, '\n')))
         end
@@ -128,17 +235,61 @@ CommandsAPIService.PostCommand {
     end
 }
 
-table.insert(GLOBAL.GenericConnections, Players.LocalPlayer.CharacterAdded:Connect(OnCharacterAdded))
-table.insert(GLOBAL.GenericConnections, UserInputService.InputBegan:Connect(function(input: InputObject, gpe: boolean)
+do
+    RunService:BindToRenderStep("FlyUpdate", Enum.RenderPriority.Input.Value - 1, function(dt)
+        if canPlayerFly and flightStateEnabled then
+            if humanoidRootPart or (humanoid and humanoid.Health <= 0) then
+                local dir = Vector3.zero
+                if flyInputs[Enum.KeyCode.A] then
+                    dir -= Vector3.xAxis
+                end
+                if flyInputs[Enum.KeyCode.D] then
+                    dir += Vector3.xAxis
+                end
+                if flyInputs[Enum.KeyCode.W] then
+                    dir -= Vector3.zAxis
+                end
+                if flyInputs[Enum.KeyCode.S] then
+                    dir += Vector3.zAxis
+                end
+                local input = workspace.CurrentCamera.CFrame:VectorToObjectSpace(dir)
+                if input:Dot(input) > 0 then
+                    input = input.Unit*flightSpeed
+                end
+                local pos = humanoidRootPart.Position + input
+                flyMovers.position.Position = pos
+                flyMovers.orientation.CFrame = CFrame.lookAt(Vector3.zero, workspace.CurrentCamera.CFrame.LookVector)
+            end
+        end
+    end)
+    table.insert(GLOBAL.GenericCleanup, function()
+        RunService:UnbindFromRenderStep("FlyUpdate")
+    end)
+end
+table.insert(GLOBAL.GenericCleanup, Players.LocalPlayer.CharacterAdded:Connect(OnCharacterAdded))
+table.insert(GLOBAL.GenericCleanup, UserInputService.InputBegan:Connect(function(input: InputObject, gpe: boolean)
     if gpe then return end
     if input.KeyCode == GLOBAL.GenericKeybinds.Teleport then
         if canPlayerTeleportToMouse then
             if humanoidRootPart then
+                if humanoid.Health <= 0 then
+                    return
+                end
                 local result = RaycastToMouse()
                 local position = result.Position+result.Normal*2
                 humanoidRootPart.CFrame = CFrame.new(position) * CFrame.lookAt(Vector3.zero, humanoidRootPart.CFrame.LookVector)
             end
         end
+    elseif input.KeyCode == GLOBAL.GenericKeybinds.Fly then
+        toggleFlyState(not flightStateEnabled)
+    elseif flyInputs[input.KeyCode] ~= nil then
+        flyInputs[input.KeyCode] = true
+    end
+end))
+table.insert(GLOBAL.GenericCleanup, UserInputService.InputEnded:Connect(function(input: InputObject, gpe: boolean)
+    if gpe then return end
+    if flyInputs[input.KeyCode] ~= nil then
+        flyInputs[input.KeyCode] = false
     end
 end))
 
